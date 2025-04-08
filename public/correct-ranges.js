3efc9c68f7a5e37eb9ddb2c3b1747a443d1ae94d
Dmitry Baryshev <dima8w@gmail.com>
2025-04-08 15:30:53 +0300

    Correct ranges automatically

---
+++ public/correct-ranges.js
@@ -0,0 +1,20 @@
+﻿// Correct ranges automatically
+//
+$(document).ready(function() {
+    $("input[type='number'][min][max]").on("blur", function() {
+        const $input = $(this);
+        const value = parseFloat($input.val());
+        const min = parseFloat($input.attr("min"));
+        const max = parseFloat($input.attr("max"));
+
+        if (isNaN(value)) {
+            $input.val(min);
+        } else {
+            if (value < min) {
+                $input.val(min);
+            } else if (value > max) {
+                $input.val(max);
+            }
+        }
+    });
+});
